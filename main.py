29db2a859291702ec18ac942f4da1fb9fc1c2d72
Benjamin Gallagher <bishopayak@gmail.com>
2020-11-07 12:18:36 -0500

    Added Hello World

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import discord
 
-token = '' #TODO: Put discord bot token in here
+token = 'Nzc0NjgwMzExMTk1ODkzODAw.X6bTQw.yjYLZGEBc6PVOWS5PiyXdaNsKVg'
 client = discord.Client() #Creates the bots client
+
+@client.event()
+async def on_message(message):
+    #So the bot doesnt reply to itself
+    if message.author == client.user:
+        return
+
+    if(message.content == '!hello'):
+        await client.send_message(message.channel, 'Hello World')
+
+client.run(token)#Runs the bot
