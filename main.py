ad793519f43b967821211f3808e27e378fd0f49e
Benjamin Gallagher <bishopayak@gmail.com>
2020-11-07 13:56:25 -0500

    Added simple JSON saves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import json
 
 token = 'Nzc0NjgwMzExMTk1ODkzODAw.X6bTQw.yjYLZGEBc6PVOWS5PiyXdaNsKVg'
 client = commands.Bot(command_prefix = '$')
@@ -17,6 +18,18 @@ async def whoami(ctx):
     name = str(ctx.author).split('#')
     await ctx.send(name[0])
 
+@client.command()
+async def startgame(ctx):
+    data = json.load('saves.txt') #Loads the JSON file and saves it as a var called data
+    #TODO: Write a statement that checks if a user already has save data and ask to confirm if they want to erase previous save
+    #TODO: Make sure when a user is confirming to delete the save that it is the same user and no one else can confirm a delete save
+    data['users'].append({
+        'name': f'{ctx.author}'
+    })
+
+    #Writes to the JSON file
+    with open('saves.txt', 'w') as outfile:
+        json.dump(data, outfile)
 
 
 client.run(token)#Runs the bot
