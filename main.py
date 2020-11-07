7e1bc692074789ec20e6f0a2d8a82f273a0f610c
nekozye <jc865@njit.edu>
2020-11-07 14:45:05 -0500

    Added action command Added loading save functionality (only json, no formatting) Added making new save file functionality (creates new blank templated json)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import discord
 from discord.ext import commands
 import json
+import datetime
+import os
 
 token = 'Nzc0NjgwMzExMTk1ODkzODAw.X6bTQw.yjYLZGEBc6PVOWS5PiyXdaNsKVg'
+
+save_folder = os.path.abspath(os.getcwd())+"/saves"
 client = commands.Bot(command_prefix = '$')
 
 @client.command()
@@ -19,17 +23,60 @@ async def whoami(ctx):
     await ctx.send(name[0])
 
 @client.command()
-async def startgame(ctx):
-    data = json.load('saves.txt') #Loads the JSON file and saves it as a var called data
-    #TODO: Write a statement that checks if a user already has save data and ask to confirm if they want to erase previous save
-    #TODO: Make sure when a user is confirming to delete the save that it is the same user and no one else can confirm a delete save
-    data['users'].append({
-        'name': f'{ctx.author}'
-    })
+async def action(ctx):
+    # Separate Logic that just retrieves jsonfile read based on the tag (str)
+    # If there is no file, create one
+    # Deletion of save is handled with different command
+    def loadsave(tag):
 
-    #Writes to the JSON file
-    with open('saves.txt', 'w') as outfile:
-        json.dump(data, outfile)
+        if not os.path.isdir(save_folder):
+            os.mkdir(save_folder)
+
+        save_path_guess = save_folder + "/" + tag + ".json"
+
+        if not os.path.isdir(save_path_guess):
+            newsave(tag)
+
+        json_read = "file_err"
+
+        #TODO: Needs a way to read the savefile after it has created. The JSON file is there, but it is not formatted back to objects
+        with open(save_path_guess) as f:
+            json_read = json.load(f)
+
+        print(json_read)
+
+    # Creation of new file
+    # Savefile contains in json file format
+    # name
+    # date and time of file creation
+    # date and time of last action (used for
+    # last action (string)
+    # current location (string)
+    # funds (number, this will be some sort of number that will be later calculated to current country's currency)
+    # infection (number, days till recover. could be game over if this is true, but chance based)
+    # symptoms (list)
+    # tested (list)
+    # items (list)
+    # equipment (list)
+    def newsave(tag):
+        save_path_guess = save_folder + "/" + tag + ".json"
+        json_file = {
+            'name': tag,
+            'file_creation': datetime.datetime.now(),
+            'last_action': "",
+            'current_loc': "",
+            'fund': 0,
+            'infection': False,
+            'symptoms': {},
+            'tested': {},
+            'items': {},
+            'equipment': {}
+        }
+
+        with open(save_path_guess, 'w') as f:
+            json.dumps(json_file, f)
+
+    loadsave(ctx.author)
 
 
 client.run(token)#Runs the bot
