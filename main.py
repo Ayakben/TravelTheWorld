72a299a284f0556c7e012066a2833aa073877059
Benjamin Gallagher <bishopayak@gmail.com>
2020-11-07 15:15:11 -0500

    Figured out reactions

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ async def ping(ctx):
 @client.command()
 async def echo(ctx, *, message):
     await ctx.send(message)
+    print(message)
 
 @client.command()
 async def whoami(ctx):
@@ -23,6 +24,7 @@ async def startgame(ctx):
     data = json.load('saves.txt') #Loads the JSON file and saves it as a var called data
     #TODO: Write a statement that checks if a user already has save data and ask to confirm if they want to erase previous save
     #TODO: Make sure when a user is confirming to delete the save that it is the same user and no one else can confirm a delete save
+
     data['users'].append({
         'name': f'{ctx.author}'
     })
@@ -31,5 +33,16 @@ async def startgame(ctx):
     with open('saves.txt', 'w') as outfile:
         json.dump(data, outfile)
 
+@client.command()
+async def action(ctx):
+    def check(m):
+        return m.content == 'hello' and m.channel == ctx.channel and m.author == ctx.author
+    reaction = await client.wait_for('reaction_add', check = check)
+    await ctx.send('yo')
+
+@client.command()
+async def react(ctx):
+    message = await ctx.send('I want to kill myself')
+    await message.add_reaction('👍')
 
 client.run(token)#Runs the bot
