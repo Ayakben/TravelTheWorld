591a61824804739b0196d0fb6592f6a6758f214e
Benjamin Gallagher <bishopayak@gmail.com>
2020-11-07 12:36:04 -0500

    Fixed Messages Sending

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import discord
 token = 'Nzc0NjgwMzExMTk1ODkzODAw.X6bTQw.yjYLZGEBc6PVOWS5PiyXdaNsKVg'
 client = discord.Client() #Creates the bots client
 
-@client.event()
+
+@client.event
 async def on_message(message):
     #So the bot doesnt reply to itself
     if message.author == client.user:
         return
 
     if(message.content == '!hello'):
-        await client.send_message(message.channel, 'Hello World')
+        await message.channel.send('Hello World')
 
 client.run(token)#Runs the bot
