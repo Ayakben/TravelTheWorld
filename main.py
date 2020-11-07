ed7c261ad2b3d9bf757a639c3b5c6ba1da298026
Benjamin Gallagher <bishopayak@gmail.com>
2020-11-07 13:22:23 -0500

    Added test commands

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ client = commands.Bot(command_prefix = '$')
 
 @client.command()
 async def ping(ctx):
-    await ctx.send('Pong!')
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def echo(ctx, *, message):
+    await ctx.send(message)
+
+@client.command()
+async def whoami(ctx):
+    name = str(ctx.author).split('#')
+    await ctx.send(name[0])
+
+
 
 client.run(token)#Runs the bot
